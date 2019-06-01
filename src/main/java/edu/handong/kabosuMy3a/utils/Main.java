da58abfe778ed8971f60873b72ad4eef62729540
kabosuMy3a <my3a3380@gmail.com>
2019-06-01 15:16:34 +0900

    start

---
+++ src/main/java/edu/handong/kabosuMy3a/utils/Main.java
@@ -0,0 +1,11 @@
+package edu.handong.kabosuMy3a.utils ;
+
+public class Main{
+
+	public static void main(String[] args){
+		
+		kabosuBookDataMaker kbdm = new kabosuBookDataMaker() ;
+		
+		kdbm.run(args);
+	}
+}
---
+++ src/main/java/edu/handong/kabosuMy3a/utils/kabosuBookDataMaker.java
@@ -0,0 +1,11 @@
+package edu.handong.kabosuMy3a.utils ;
+
+
+public class kabosuBookDataMaker{
+
+
+
+
+
+
+}
